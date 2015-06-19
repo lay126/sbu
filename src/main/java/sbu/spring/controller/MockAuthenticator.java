5a672c2bd5375fdd6d853931ad02752d9de11cc1
YeoeunJeong <YeoeunJeong@YeoeunJeong-PC.lan>
2015-06-19 17:55:09 +0900

    MockAuthenticator.java RegistMemberController.java 두개 추가

---
+++ src/main/java/sbu/spring/controller/MockAuthenticator.java
@@ -0,0 +1,12 @@
+package sbu.spring.controller;
+
+public class MockAuthenticator implements Authenticator {
+
+	@Override
+	public void authenticate(String id, String password) {
+		if (!id.equals("madvirus")) {
+			throw new AuthenticationException("invalid id "+id);
+		}
+	}
+
+}
