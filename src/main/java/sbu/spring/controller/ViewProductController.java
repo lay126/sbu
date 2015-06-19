8bfa7a0b0e3ee70c8b8ecd8212ca7c7e9efb49e7
kyoung eun <kkee2@naver.com>
2015-06-19 21:16:05 +0900

    연결시켜줭

--- src/main/java/sbu/spring/controller/ViewProductController.java
+++ src/main/java/sbu/spring/controller/ViewProductController.java
@@ -1,5 +1,31 @@
 package sbu.spring.controller;
 
+import sbu.spring.domain.Product;
+import sbu.spring.service.sBuFacade;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class ViewProductController {
+
+	private sBuFacade sBu;
+
+	@Autowired
+	public void setPetStore(sBuFacade sBu) {
+		this.sBu = sBu;
+	}
+
+	@RequestMapping("/shop/viewItem.do")
+	public String handleRequest(@RequestParam("ProductNum") int productNum,
+			ModelMap model) throws Exception {
+		Product product = this.sBu.getProduct(productNum);
+		//model.put("product", product);
+		model.put("product", product.getProductName());
+		return "Product";
+	}
 
 }
