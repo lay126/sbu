c950c6c21db8a3381a1b6e11c1ad747db996594f
YeoeunJeong <YeoeunJeong@YeoeunJeong-PC.lan>
2015-06-19 22:11:45 +0900

    productDao

--- src/main/java/sbu/spring/dao/mybatis/MybatisProductDao.java
+++ src/main/java/sbu/spring/dao/mybatis/MybatisProductDao.java
@@ -1,9 +1,25 @@
 package sbu.spring.dao.mybatis;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import sbu.spring.dao.ProductDao;
+import sbu.spring.dao.mybatis.mapper.ProductMapper;
 import sbu.spring.domain.Product;
 
-public class MybatisProductDao {
-	//sbu
-	
+public class MybatisProductDao implements ProductDao {
+	@Autowired
+	private ProductMapper productMapper;
+
+	@Override
+	public List<Product> getProductListByCategory(int productCateNum) {
+		return productMapper.getProductListByCategory(productCateNum);
+	}
+
+	@Override
+	public Product getProduct(int productNum) {
+		return productMapper.getProduct(productNum);
+	}
+
 }
