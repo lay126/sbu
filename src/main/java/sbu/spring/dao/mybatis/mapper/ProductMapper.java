26f74f4978d972b6d7227b8d64bbff0a3123a180
kyoung eun <kkee2@naver.com>
2015-06-19 22:10:28 +0900

    mapper

--- src/main/java/sbu/spring/dao/mybatis/mapper/ProductMapper.java
+++ src/main/java/sbu/spring/dao/mybatis/mapper/ProductMapper.java
@@ -1,5 +1,13 @@
 package sbu.spring.dao.mybatis.mapper;
+import sbu.spring.domain.Product;
+import java.util.List;
 
-public class ProductMapper {
-	// sbu
+public interface ProductMapper {
+
+	  List<Product> getProductListByCategory(int productCateNum);
+
+	  Product getProduct(int productNum);
+
+	  //List<Product> searchProductList(String keywords);
+
 }
