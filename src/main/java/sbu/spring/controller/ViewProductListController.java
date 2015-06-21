0e690e1f29433c6a28757cd5738d291fe883d667
YeoeunJeong <YeoeunJeong@192.168.139.41>
2015-06-21 10:15:03 +0900

    viewProductListController

--- src/main/java/sbu/spring/controller/ViewProductListController.java
+++ src/main/java/sbu/spring/controller/ViewProductListController.java
@@ -1,34 +1,35 @@
 package sbu.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import sbu.spring.domain.Product;
 import sbu.spring.service.sBuFacade;
 
 @Controller
 public class ViewProductListController {
-	Product product = new Product();
+
+	PagedListHolder<Product> productList = null;
 	private sBuFacade sBuf;
-	
+
 	@Autowired
 	public void setSBuf(sBuFacade sBuf) {
 		this.sBuf = sBuf;
 	}
-	
+
 	@RequestMapping("/jsp/viewProductList.do")
-	public String handleRequest (
-			@RequestParam(value = "productCateNum") int productCateNum, 
+	public String handleRequest(
+			@RequestParam(value = "productCateNum") int productCateNum,
 			ModelMap model) throws Exception {
-		
+		productList = new PagedListHolder<Product>(
+				this.sBuf.getProductListByCategory(productCateNum));
+		productList.setPageSize(10);
+		model.put("productList", productList);
 		
 		return "ProductList";
-		
 	}
 }
-
-
